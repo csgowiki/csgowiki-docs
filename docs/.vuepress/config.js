9c9aa58b5b29d4014f67157f19757a69909b7a40
hx-w <herixth@163.com>
2021-06-08 23:18:57 +0800

    vuepress arch

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,73 @@
+module.exports = {
+    title: 'CSGOWiki 帮助中心',
+    description: 'CSGOWiki 帮助中心',
+    base: '/',
+    plugins: {
+        '@vuepress/search': {
+            searchMaxSuggestions: 10
+        },
+        '@vuepress/back-to-top': true,
+        '@vuepress/pwa': {
+            serviceWorker: true,
+            updatePopup: {
+                message: "发现新的内容",
+                buttonText: "刷新",
+            }
+        }
+    },
+    head: [
+        [
+            'script',
+            {
+                async: true,
+                src: 'https://www.googletagmanager.com/gtag/js?id=G-CZJ8X185MG',
+            }
+        ],
+        [
+            'script',
+            {},
+            [
+                "window.dataLayer = window.dataLayer || [];\nfunction gtag(){dataLayer.push(arguments);}\ngtag('js', new Date());\ngtag('config', 'G-CZJ8X185MG');",
+            ],
+        ],
+    ],
+    themeConfig: {
+        search: true,
+        searchMaxSuggestions: 5,
+        smoothScroll: true,
+        lastUpdated: '上一次编辑',
+        docsDir: 'docs',
+        docsBranch: 'main',
+        repo: 'hx-w/CSGOWiki-Plugins',
+        repoLabel: 'GitHub',
+        docsRepo: 'hx-w/csgowiki-docs',
+        editLinks: true,
+        editLinkText: '编辑此页',
+        displayAllHeaders: false,
+        sidebarDepth: 2,
+        activeHeaderLinks: true,
+        nav: [
+            { text: '主页', link: '/' },
+            { text: '导航', link: '/guide/' },
+            { text: '插件文档', link: '/plugins/' },
+            { text: 'Demo解析', link: '/demo-parser/' },
+            { text: '消息中继服务', link: '/message-channel/' },
+            { text: 'FAQ', link: '/faq/' }
+        ],
+        sidebar: {
+            '/guide/': [
+            ],
+            '/api/': [
+                '',
+            ],
+            '/event/': [
+                '',
+            ],
+            '/cqcode/': [
+                '',
+            ],
+            '/faq/': [
+            ]
+        }
+    }
+}
---
+++ docs/README.md
@@ -0,0 +1,16 @@
+---
+home: true
+heroImage: https://ibed.csgowiki.top/new_avatar
+heroText: CSGOWiki
+tagline: CS:GO相关的Web服务实现
+actionText: 快速开始 →
+actionLink: /guide/
+features:
+- title: 社区道具管理
+  details: 以图文方式展示，并在游戏服务器内进行重现
+- title: 职业比赛解析
+  details: 每日自动解析职业比赛demo，轻而易举获取你想要的的信息
+- title: 消息中继服务
+  details: 实现游戏服务器与QQ群聊消息互通，管理服务器变得更加方便
+footer: MIT Licensed | Copyright © 2021-present CarOL
+---

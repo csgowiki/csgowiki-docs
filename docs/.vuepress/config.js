66355fbe1d75fc396ef3ff1f9360ccc878522793
hx-w <herixth@163.com>
2021-06-09 11:24:53 +0800

    文档结构完善

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -51,9 +51,19 @@ module.exports = {
             { text: '主页', link: '/' },
             { text: '导航', link: '/guide/' },
             { text: '插件文档', link: '/plugins/' },
-            { text: 'Demo解析', link: '/demo-parser/' },
+            { text: 'Demo道具解析', link: '/demo-parser/' },
             { text: '消息中继服务', link: '/message-channel/' },
             { text: 'FAQ', link: '/faq/' }
         ],
-    }
+        sidebar: {
+            '/plugins/': [
+                '',
+                'quick_start',
+                'update',
+                'config',
+                'menu',
+                'admin',
+            ]
+        }
+    },
 }
---
+++ docs/guide/README.md
@@ -0,0 +1,18 @@
+# 简介
+
+**CSGOWiki是面向CS:GO玩家的一体化Web服务。**
+
+## CSGOWiki主站
+
+![Uptime CI](https://github.com/website-200/csgowiki-bot/workflows/Uptime%20CI/badge.svg)![Response Time CI](https://github.com/website-200/csgowiki-bot/workflows/Response%20Time%20CI/badge.svg)
+
+
+[**Visit CSGOWiki →**](https://csgowiki.top)
+
+
+## 服务列表
+
+
+
+## 开发&维护
+

1f97706b37b589bc728944970c25125f26248f18
Herixth <17361019875@163.com>
2021-08-29 22:15:34 +0800

    new table format

--- docs/.vuepress/components/ServerTables.vue
+++ docs/.vuepress/components/ServerTables.vue
@@ -2,34 +2,45 @@
   <a-spin :spinning="spinning" size="large">
     <a-tabs tab-position="left">
       <a-tab-pane v-for="item in data" :key="item.remark" :tab="item.remark">
-        <div id="components-table-demo-size" v-if="item.players_info.length > 0">
-          <a-table
-            :columns="columns"
+        <div
+          id="components-table-demo-size"
+          v-if="item.players_info.length > 0"
+        >
+          <a-list
+            class="demo-loadmore-list"
+            item-layout="horizontal"
             :data-source="item.players_info"
-            :pagination="false"
-            :row-key="(data) => data.client_id"
           >
-            <a slot="name" slot-scope="text">{{ text }}</a>
-            <span slot="customTitle">Player Name</span>
-            <span slot="ping" slot-scope="ping">
-              <a-tag
-                :color="
-                  ping > 50 ? 'volcano' : ping < 10 ? 'green' : 'geekblue'
-                "
-              >
-                {{ ping }}ms
-              </a-tag>
-            </span>
-            <span slot="steamid" slot-scope="steamid">
-              <a href="#">{{ steamid }}</a>
-            </span>
-          </a-table>
-          
+            <a-list-item slot="renderItem" slot-scope="item">
+              <a-list-item-meta :description="item.steamid">
+                <a slot="title" :href="url + item.steamid">{{
+                  item.player_name
+                }}</a>
+                <a-avatar
+                  slot="avatar"
+                  src="https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png"
+                />
+              </a-list-item-meta>
+              <span>
+                <a-tag
+                  :color="
+                    item.ping > 50
+                      ? 'volcano'
+                      : item.ping < 10
+                      ? 'green'
+                      : 'geekblue'
+                  "
+                >
+                  {{ item.ping }}ms
+                </a-tag>
+              </span>
+            </a-list-item>
+          </a-list>
         </div>
         <div id="components-table-demo-size" v-else>
           <a-result title="服务器空闲中，快来玩吧！">
             <template #icon>
-              <a-icon type="coffee"/>
+              <a-icon type="coffee" />
             </template>
             <template #extra>
               <a-button type="primary" icon="right"> 连入服务器 </a-button>
@@ -59,35 +70,12 @@
 <script>
 import reqwest from "reqwest";
 
-const columns = [
-  {
-    key: "player_name",
-    dataIndex: "player_name",
-    slots: { title: "customTitle" },
-    scopedSlots: { customRender: "name" },
-    width: "20%",
-  },
-  {
-    key: "steamid",
-    title: "Steam ID",
-    dataIndex: "steamid",
-    scopedSlots: { customRender: "steamid" },
-    width: "25%",
-  },
-  {
-    key: "ping",
-    title: "Ping",
-    dataIndex: "ping",
-    scopedSlots: { customRender: "ping" },
-    width: "10%",
-  },
-];
 export default {
   data() {
     return {
       data: [],
       spinning: true,
-      columns,
+      url: "https://steamcommunity.com/profiles/",
     };
   },
   mounted() {
@@ -100,15 +88,15 @@ export default {
     getData(callback) {
       reqwest({
         url: "https://s1.csgowiki.top:8080/api/server_info?qq_group=762993431",
-        type: 'json',
-        method: 'get',
-        contentType: 'application/json',
-        success: res => {
+        type: "json",
+        method: "get",
+        contentType: "application/json",
+        success: (res) => {
           callback(res);
         },
-        error: res => {
+        error: (res) => {
           this.$message.error("服务器列表加载失败：" + res.message);
-        }
+        },
       });
     },
     handleTableChange(filters, sorter) {
@@ -120,7 +108,7 @@ export default {
     },
     fetch(params = {}) {
       this.spinning = true;
-      this.getData(res => {
+      this.getData((res) => {
         this.spinning = false;
         this.data = res.results;
       });
